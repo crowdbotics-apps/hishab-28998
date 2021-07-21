0366382268032558e0ff0110580bc8b9692eed13
Crowdbotics <team@crowdbotics.com>
2021-07-21 09:19:59 +0000

    Initial commit.

---
+++ backend/hishab_28998/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for hishab_28998 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hishab_28998.settings')
+
+application = get_wsgi_application()
